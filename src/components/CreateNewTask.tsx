40ec95bd77fe6174cfde945ab9cca3a6a2c5cc59
Diogo Meneres <dmeneres@gmail.com>
2023-02-08 19:46:46 +0000

    changed last two images scr

--- src/components/CreateNewTask.tsx
+++ src/components/CreateNewTask.tsx
@@ -1,41 +1,38 @@
-import { ChangeEvent, FormEvent, useState } from 'react'
-import styles from './CreateNewTask.module.css'
+import { ChangeEvent, FormEvent, useState } from "react";
+import styles from "./CreateNewTask.module.css";
 
 interface Props {
   onAddTask: (taskTitle: string) => void;
 }
 
 export function CreateNewTask({ onAddTask }: Props) {
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
 
   function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
-    setTitle(event.target.value)
-    console.log(event)
+    setTitle(event.target.value);
+    console.log(event);
   }
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
 
     onAddTask(title);
-    setTitle('')
-  } 
+    setTitle("");
+  }
 
   return (
-    <form 
-      onSubmit={handleSubmit} 
-      className={styles.createNewTaskContainer}
-    >
-      <input 
-        onChange={onChangeTitle} 
-        className={styles.taskInput} 
-        placeholder="Create new task" 
+    <form onSubmit={handleSubmit} className={styles.createNewTaskContainer}>
+      <input
+        onChange={onChangeTitle}
+        className={styles.taskInput}
+        placeholder="Create new task"
         type="text"
         value={title}
       />
       <button className={styles.createButton} type="submit">
         <p>Create</p>
-        <img src="../src/assets/plus-create-button.svg" alt="" />
+        <img src="/static/images/plus-create-button.svg" alt="" />
       </button>
     </form>
-  )
+  );
 }
